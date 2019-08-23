31603f0b36e25f4f735e0d9090f0b92cd9c1e6fb
Sebastien Lebreton <sebleb@microsoft.com>
2019-08-23 09:24:57 -0700

    Remove unused usings

--- src/Microsoft.Unity.Analyzers/EmptyUnityMessage.cs
+++ src/Microsoft.Unity.Analyzers/EmptyUnityMessage.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Immutable;
+﻿using System.Collections.Immutable;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
--- src/Microsoft.Unity.Analyzers/ReadonlySerializeFieldSuppressor.cs
+++ src/Microsoft.Unity.Analyzers/ReadonlySerializeFieldSuppressor.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Immutable;
 using System.Linq;
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.Unity.Analyzers.Resources;
 
