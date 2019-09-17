b9365ca4b03fa2389b6346a19b00affcd284cf4d
Sebastien Lebreton <sebleb@microsoft.com>
2019-09-17 17:26:01 +0000

    Merged PR 202370: Improve TagComparison
    Improve TagComparison to support those scenarios:
    
    ```
    private void Update()
        {
    		if (tag == ""foo"")
    			return;
    
    		if (null != tag)
    			return;
    
    		if (this.tag == null)
    			return;
    
    		if (null != another.tag)
    			return;
    
    		if (tag.Equals(""bar""))
    			return;
    
    		if (!this.tag.Equals(null))
    			return;
    
    		if (""foo"".Equals(tag))
    			return;
    
    		if (Equals(null, tag))
    			return;
    
    		if (object.Equals(another.tag, null))
    			return;
    	}
    ```
    With:
    
    ```
    private void Update()
        {
    		if (CompareTag(""foo""))
    			return;
    
    		if (!CompareTag(null))
    			return;
    
    		if (this.CompareTag(null))
    			return;
    
    		if (!another.CompareTag(null))
    			return;
    
    		if (CompareTag(""bar""))
    			return;
    
    		if (!this.CompareTag(null))
    			return;
    
    		if (CompareTag(""foo""))
    			return;
    
    		if (CompareTag(null))
    			return;
    
    		if (another.CompareTag(null))
    			return;
    	}
    ```
    
    Related work items: #982807

--- src/Microsoft.Unity.Analyzers.Tests/TagComparisonTests.cs
+++ src/Microsoft.Unity.Analyzers.Tests/TagComparisonTests.cs
@@ -39,6 +39,130 @@ public class Camera : MonoBehaviour
 		}
 
 		[Fact]
+		public async Task TagAsIdentifierInvoke()
+		{
+			const string test = @"
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    private void Update()
+    {
+        Debug.Log(tag.Equals(""tag1""));
+    }
+}
+";
+
+			var diagnostic = Verify.Diagnostic().WithLocation(8, 19);
+
+			const string fixedTest = @"
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    private void Update()
+    {
+        Debug.Log(CompareTag(""tag1""));
+    }
+}
+";
+			await Verify.VerifyCodeFixAsync(test, diagnostic, fixedTest);
+		}
+
+		[Fact]
+		public async Task TagAsIdentifierNeqInvoke()
+		{
+			const string test = @"
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    private void Update()
+    {
+        Debug.Log(!tag.Equals(""tag1""));
+    }
+}
+";
+
+			var diagnostic = Verify.Diagnostic().WithLocation(8, 20);
+
+			const string fixedTest = @"
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    private void Update()
+    {
+        Debug.Log(!CompareTag(""tag1""));
+    }
+}
+";
+			await Verify.VerifyCodeFixAsync(test, diagnostic, fixedTest);
+		}
+
+		[Fact]
+		public async Task TagAsIdentifierThisPrefix()
+		{
+			const string test = @"
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    private void Update()
+    {
+        Debug.Log(this.tag == ""tag1"");
+    }
+}
+";
+
+			var diagnostic = Verify.Diagnostic().WithLocation(8, 19);
+
+			const string fixedTest = @"
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    private void Update()
+    {
+        Debug.Log(this.CompareTag(""tag1""));
+    }
+}
+";
+			await Verify.VerifyCodeFixAsync(test, diagnostic, fixedTest);
+		}
+
+		[Fact]
+		public async Task TagAsIdentifierNeqNullRhs()
+		{
+			const string test = @"
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    private void Update()
+    {
+        Debug.Log(null != tag);
+    }
+}
+";
+
+			var diagnostic = Verify.Diagnostic().WithLocation(8, 19);
+
+			const string fixedTest = @"
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    private void Update()
+    {
+        Debug.Log(!CompareTag(null));
+    }
+}
+";
+			await Verify.VerifyCodeFixAsync(test, diagnostic, fixedTest);
+		}
+
+		[Fact]
 		public async Task TagProperty()
 		{
 			const string test = @"
@@ -74,6 +198,37 @@ public class Camera : MonoBehaviour
 		}
 
 		[Fact]
+		public async Task TagPropertyForGameObject()
+		{
+			const string test = @"
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    private void Update()
+    {
+        Debug.Log(gameObject.tag == ""tag2"");
+    }
+}
+";
+
+			var diagnostic = Verify.Diagnostic().WithLocation(8, 19);
+
+			const string fixedTest = @"
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    private void Update()
+    {
+        Debug.Log(gameObject.CompareTag(""tag2""));
+    }
+}
+";
+			await Verify.VerifyCodeFixAsync(test, diagnostic, fixedTest);
+		}
+
+		[Fact]
 		public async Task TagAsIdentifierRhs()
 		{
 			const string test = @"
@@ -84,6 +239,37 @@ public class Camera : MonoBehaviour
     private void Update()
     {
         Debug.Log(""tag3"" == tag);
+    }
+}
+";
+
+			var diagnostic = Verify.Diagnostic().WithLocation(8, 19);
+
+			const string fixedTest = @"
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    private void Update()
+    {
+        Debug.Log(CompareTag(""tag3""));
+    }
+}
+";
+			await Verify.VerifyCodeFixAsync(test, diagnostic, fixedTest);
+		}
+
+		[Fact]
+		public async Task TagAsIdentifierRhsInvoke()
+		{
+			const string test = @"
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    private void Update()
+    {
+        Debug.Log(""tag3"".Equals(tag));
     }
 }
 ";
@@ -138,5 +324,115 @@ public class Camera : MonoBehaviour
 ";
 			await Verify.VerifyCodeFixAsync(test, diagnostic, fixedTest);
 		}
+
+		[Fact]
+		public async Task TagPropertyNeqNullRhs()
+		{
+			const string test = @"
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public Transform transform;
+
+    private void Update()
+    {
+        Debug.Log(null != transform.tag);
+    }
+}
+";
+
+			var diagnostic = Verify.Diagnostic().WithLocation(10, 19);
+
+			const string fixedTest = @"
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public Transform transform;
+
+    private void Update()
+    {
+        Debug.Log(!transform.CompareTag(null));
+    }
+}
+";
+			await Verify.VerifyCodeFixAsync(test, diagnostic, fixedTest);
+		}
+
+		[Fact]
+		public async Task ObjectEqualsExplicit()
+		{
+			const string test = @"
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public Transform transform;
+
+    private void Update()
+    {
+        if (object.Equals(transform.tag, null))
+            return;
+    }
+}
+";
+
+			var diagnostic = Verify.Diagnostic().WithLocation(10, 13);
+
+			const string fixedTest = @"
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public Transform transform;
+
+    private void Update()
+    {
+        if (transform.CompareTag(null))
+            return;
+    }
+}
+";
+			await Verify.VerifyCodeFixAsync(test, diagnostic, fixedTest);
+		}
+
+		[Fact]
+		public async Task ObjectEqualsImplicit()
+		{
+			const string test = @"
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public Transform transform;
+
+    private void Update()
+    {
+        if (Equals(null, transform.tag))
+            return;
+    }
+}
+";
+
+			var diagnostic = Verify.Diagnostic().WithLocation(10, 13);
+
+			const string fixedTest = @"
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public Transform transform;
+
+    private void Update()
+    {
+        if (transform.CompareTag(null))
+            return;
+    }
+}
+";
+			await Verify.VerifyCodeFixAsync(test, diagnostic, fixedTest);
+		}
+
 	}
 }
--- src/Microsoft.Unity.Analyzers/TagComparison.cs
+++ src/Microsoft.Unity.Analyzers/TagComparison.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Immutable;
+﻿using System;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -9,7 +10,6 @@ using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.Unity.Analyzers.Resources;
-using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
 
 namespace Microsoft.Unity.Analyzers
 {
@@ -33,55 +33,97 @@ namespace Microsoft.Unity.Analyzers
 		{
 			context.EnableConcurrentExecution();
 			context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
-			context.RegisterSyntaxNodeAction(AnalyzeMethodDeclaration, SyntaxKind.EqualsExpression);
+			context.RegisterSyntaxNodeAction(AnalyzeBinaryExpression, SyntaxKind.EqualsExpression, SyntaxKind.NotEqualsExpression);
+			context.RegisterSyntaxNodeAction(AnalyzeInvocationExpression, SyntaxKind.InvocationExpression);
 		}
 
-		private static void AnalyzeMethodDeclaration(SyntaxNodeAnalysisContext context)
+		private static void AnalyzeInvocationExpression(SyntaxNodeAnalysisContext context)
 		{
-			var binary = (BinaryExpressionSyntax)context.Node;
+			var expr = (InvocationExpressionSyntax) context.Node;
+			var nameSyntax = GetMethodNameSyntax(expr);
 
-			if (IsTagComparison(binary.Left, binary.Right, context) || IsTagComparison(binary.Right, binary.Left, context))
-				context.ReportDiagnostic(Diagnostic.Create(Rule, binary.GetLocation()));
-		}
+			if (!IsSupportedMethod(context, nameSyntax))
+				return;
 
-		private static bool IsTagIdentifier(SimpleNameSyntax id) => id.Identifier.Text == "tag";
-
-		private static bool IsTagIdentifierAccess(SyntaxNode node)
-		{
-			switch (node)
+			var mae = expr.Expression as MemberAccessExpressionSyntax;
+			if (mae != null)
 			{
-				case IdentifierNameSyntax id:
-					if (IsTagIdentifier(id))
-						return true;
-
-					break;
-				case MemberAccessExpressionSyntax ma:
-					if (IsTagIdentifier(ma.Name))
-						return true;
-
-					break;
+				if (IsReportableExpression(context, mae.Expression))
+					context.ReportDiagnostic(Diagnostic.Create(Rule, expr.GetLocation()));
 			}
 
-			return false;
+			foreach (var argument in expr.ArgumentList.Arguments)
+			{
+				if (IsReportableExpression(context, argument.Expression))
+					context.ReportDiagnostic(Diagnostic.Create(Rule, expr.GetLocation()));
+			}
 		}
 
-		private static bool IsStringLiteral(SyntaxNode node) => node.IsKind(SyntaxKind.StringLiteralExpression);
-
-		private static bool IsTagComparison(SyntaxNode x, SyntaxNode y, SyntaxNodeAnalysisContext context)
+		private static void AnalyzeBinaryExpression(SyntaxNodeAnalysisContext context)
 		{
-			if (!IsStringLiteral(y))
+			var expr = context.Node as BinaryExpressionSyntax;
+			if (expr == null)
+				return;
+
+			if (IsReportableExpression(context, expr.Left)
+			    || IsReportableExpression(context, expr.Right))
+				context.ReportDiagnostic(Diagnostic.Create(Rule, expr.GetLocation()));
+		}
+
+		private static bool IsSupportedMethod(SyntaxNodeAnalysisContext context, ExpressionSyntax nameSyntax)
+		{
+			if (nameSyntax == null)
 				return false;
 
-			if (!IsTagIdentifierAccess(x))
+			var symbolInfo = context.SemanticModel.GetSymbolInfo(nameSyntax);
+			var symbol = symbolInfo.Symbol as IMethodSymbol;
+			if (symbol == null)
 				return false;
 
-			var symbolInfo = context.SemanticModel.GetSymbolInfo(x);
-			var property = symbolInfo.Symbol as IPropertySymbol;
-			if (property == null)
+			if (symbol.Name != "Equals" || symbol.Parameters.Length > 2)
 				return false;
 
-			return property.Name == "tag"
-				&& property.ContainingType.Matches(typeof(UnityEngine.Component));
+			if (!symbol.ReturnType.Matches(typeof(bool)))
+				return false;
+
+			var containgType = symbol.ContainingType;
+			return containgType.Matches(typeof(string)) || containgType.Matches(typeof(object));
+		}
+
+		private static NameSyntax GetMethodNameSyntax(InvocationExpressionSyntax expr)
+		{
+			var nameSyntax = default(NameSyntax);
+
+			var mae = expr.Expression as MemberAccessExpressionSyntax;
+			if (mae != null)
+				nameSyntax = mae.Name;
+
+			var ies = expr.Expression as IdentifierNameSyntax;
+			if (ies != null)
+				nameSyntax = ies;
+
+			return nameSyntax;
+		}
+
+		private static bool IsReportableExpression(SyntaxNodeAnalysisContext context, ExpressionSyntax expr)
+		{
+			return IsReportableExpression(context.SemanticModel, expr);
+		}
+
+		internal static bool IsReportableExpression(SemanticModel model, ExpressionSyntax expr)
+		{
+			var symbol = model.GetSymbolInfo(expr).Symbol;
+			return IsReportableSymbol(symbol);
+		}
+
+		private static bool IsReportableSymbol(ISymbol symbol)
+		{
+			var propertySymbol = symbol as IPropertySymbol;
+			if (propertySymbol == null)
+				return false;
+
+			var containingType = propertySymbol.ContainingType;
+			return propertySymbol.Name == "tag" && (containingType.Matches(typeof(UnityEngine.GameObject)) || containingType.Matches(typeof(UnityEngine.Component)));
 		}
 	}
 
@@ -96,50 +138,134 @@ namespace Microsoft.Unity.Analyzers
 		{
 			var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
 
-			var comparison = root.FindNode(context.Span)
-				.DescendantNodesAndSelf()
-				.OfType<BinaryExpressionSyntax>()
+			var node = root.FindNode(context.Span)
+				.DescendantNodesAndSelf().Where(n => n is BinaryExpressionSyntax || n is InvocationExpressionSyntax)
 				.FirstOrDefault();
 
-			if (comparison == null)
+			if (node == null)
 				return;
+
+			Func<CancellationToken, Task<Document>> action = null;
+			switch(node)
+			{
+				case BinaryExpressionSyntax bes:
+					action = ct => ReplaceBinaryExpressionAsync(context.Document, bes, ct);
+					break;
+				case InvocationExpressionSyntax ies:
+					action = ct => ReplaceInvocationExpressionAsync(context.Document, ies, ct);
+					break;
+			}
 
 			context.RegisterCodeFix(
 				CodeAction.Create(
 					Strings.TagComparisonCodeFixTitle,
-					ct => ConvertToCompareTagAsync(context.Document, comparison, ct),
-					comparison.ToFullString()),
+					action,
+					node.ToFullString()),
 				context.Diagnostics);
 		}
 
-		private static async Task<Document> ConvertToCompareTagAsync(Document document, BinaryExpressionSyntax comparison, CancellationToken cancellationToken)
+		private static async Task<Document> ReplaceInvocationExpressionAsync(Document document, InvocationExpressionSyntax expr, CancellationToken ct)
 		{
-			var root = await document.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
+			var root = await document.GetSyntaxRootAsync(ct).ConfigureAwait(false);
+			var model = await document.GetSemanticModelAsync(ct).ConfigureAwait(false);
 
-			var (literal, tagAccess) = comparison.Right.IsKind(SyntaxKind.StringLiteralExpression)
-				? (comparison.Right, comparison.Left)
-				: (comparison.Left, comparison.Right);
+			ExpressionSyntax tagExpression, otherExpression;
+			SortExpressions(model, expr, out tagExpression, out otherExpression);
 
-			var compareTag = (SyntaxNode)IdentifierName("CompareTag");
+			var replacement = BuildReplacementNode(tagExpression, otherExpression);
+			return document.WithSyntaxRoot(root.ReplaceNode(expr, replacement));
+		}
 
-			switch (tagAccess)
+		private static async Task<Document> ReplaceBinaryExpressionAsync(Document document, BinaryExpressionSyntax expr, CancellationToken ct)
+		{
+			var root = await document.GetSyntaxRootAsync(ct).ConfigureAwait(false);
+			var model = await document.GetSemanticModelAsync(ct).ConfigureAwait(false);
+
+			ExpressionSyntax tagExpression, otherExpression;
+			SortExpressions(model, expr, out tagExpression, out otherExpression);
+
+			var replacement = BuildReplacementNode(tagExpression, otherExpression);
+
+			if (expr.Kind() == SyntaxKind.NotEqualsExpression)
+				replacement = SyntaxFactory.PrefixUnaryExpression(SyntaxKind.LogicalNotExpression, replacement);
+
+			return document.WithSyntaxRoot(root.ReplaceNode(expr, replacement));
+		}
+
+		private static void SortExpressions(SemanticModel model, BinaryExpressionSyntax expr, out ExpressionSyntax tagExpression, out ExpressionSyntax otherExpression)
+		{
+			if (TagComparisonAnalyzer.IsReportableExpression(model, expr.Right))
 			{
-				case IdentifierNameSyntax _:
-					break;
-				case MemberAccessExpressionSyntax ma:
-					compareTag = MemberAccessExpression(
-						SyntaxKind.SimpleMemberAccessExpression,
-						ma.Expression,
-						(SimpleNameSyntax)compareTag);
-					break;
+				tagExpression = expr.Right;
+				otherExpression = expr.Left;
+			}
+			else
+			{
+				tagExpression = expr.Left;
+				otherExpression = expr.Right;
+			}
+		}
+
+		private static void SortExpressions(SemanticModel model, InvocationExpressionSyntax expr, out ExpressionSyntax tagExpression,
+			out ExpressionSyntax otherExpression)
+		{
+			var mae = expr.Expression as MemberAccessExpressionSyntax;
+			if (mae != null && expr.ArgumentList.Arguments.Count == 1)
+			{
+				if (TagComparisonAnalyzer.IsReportableExpression(model, mae.Expression))
+				{
+					tagExpression = mae.Expression;
+					otherExpression = expr.ArgumentList.Arguments.First().Expression;
+				}
+				else
+				{
+					tagExpression = FindReportableExpression(model, expr.ArgumentList);
+					otherExpression = mae.Expression;
+				}
+			}
+			else
+			{
+				tagExpression = FindReportableExpression(model, expr.ArgumentList);
+				// we cannot handle out parameter in lambda
+				var expression = tagExpression;
+				otherExpression = expr.ArgumentList.Arguments.First(a => !a.Expression.Equals(expression)).Expression;
+			}
+		}
+
+		private static ExpressionSyntax FindReportableExpression(SemanticModel model, ArgumentListSyntax argumentList)
+		{
+			return argumentList
+				.Arguments
+				.Where(argument => TagComparisonAnalyzer.IsReportableExpression(model, argument.Expression))
+				.Select(argument => argument.Expression).FirstOrDefault();
+		}
+
+		private static ExpressionSyntax BuildReplacementNode(ExpressionSyntax tagExpression, ExpressionSyntax otherExpression)
+		{
+			var CompareTagIdentifier = SyntaxFactory.IdentifierName("CompareTag");
+			ExpressionSyntax invocation = CompareTagIdentifier;
+			var mae = tagExpression as MemberAccessExpressionSyntax;
+			if (mae != null)
+			{
+				invocation = SyntaxFactory.MemberAccessExpression(
+					SyntaxKind.SimpleMemberAccessExpression,
+					mae.Expression,
+					CompareTagIdentifier);
 			}
 
-			compareTag = InvocationExpression(
-				(ExpressionSyntax)compareTag,
-				ArgumentList(
-					SeparatedList(new[] {Argument(literal)})));
-
-			return document.WithSyntaxRoot(root.ReplaceNode(comparison, compareTag));
+			var replacement = BuildInvocationExpression(invocation, otherExpression);
+			return replacement;
 		}
+
+		private static ExpressionSyntax BuildInvocationExpression(ExpressionSyntax invocation, ExpressionSyntax argument)
+		{
+			return
+				SyntaxFactory.InvocationExpression(invocation)
+					.WithArgumentList(
+						SyntaxFactory.ArgumentList(
+							SyntaxFactory.SingletonSeparatedList(
+								SyntaxFactory.Argument(argument))));
+		}
+
 	}
 }
