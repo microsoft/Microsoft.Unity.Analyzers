f63f5e8958bb9608582edc58c77bc30a8563a1c8
Sebastien Lebreton <sebastien@lebreton.fr>
2020-03-07 10:15:10 -0800

    Suppressor scaffolding (#41)
    * Add suppressor scaffolding
    
    * Update README.md
    
    * Fix indent

---
+++ src/new-analyzer/AbstractDiagnosticBuilder.cs
@@ -0,0 +1,150 @@
+﻿/*--------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *-------------------------------------------------------------------------------------------*/
+
+using System;
+using System.IO;
+
+namespace NewAnalyzer
+{
+	internal class Diagnostic
+	{
+		public string Id, Name;
+	}
+
+	internal abstract class AbstractDiagnosticBuilder
+	{
+		protected abstract string IdPrefix { get; }
+
+		protected abstract string GetResourceTemplate();
+		protected abstract string GetCodeTemplate();
+		protected abstract string GetTestTemplate();
+
+		public void Build(string name)
+		{
+			Build(new Diagnostic {Id = GetNextId(), Name = name});
+		}
+
+		public void Build(Diagnostic diagnostic)
+		{
+			CreateCodeFile(diagnostic);
+			CreateTestFile(diagnostic);
+			AddResourceEntries(diagnostic);
+		}
+
+		private void AddResourceEntries(Diagnostic diagnostic)
+		{
+			var resx = GetStringsResxPath();
+			var entries = Templatize(diagnostic, GetResourceTemplate());
+
+			var file = File.ReadAllText(resx);
+			file = file.Replace("</root>", $"{entries}");
+			File.WriteAllText(resx, file);
+		}
+
+		private static string Templatize(Diagnostic diagnostic, string template)
+		{
+			return template
+				.Replace("$(DiagnosticName)", diagnostic.Name)
+				.Replace("$(DiagnosticId)", diagnostic.Id);
+		}
+
+		private void CreateCodeFile(Diagnostic diagnostic)
+		{
+			var file = Path.Combine(GetProjectPath(), diagnostic.Name + ".cs");
+			var content = Templatize(diagnostic, GetCodeTemplate());
+			File.WriteAllText(file, content);
+		}
+
+		private void CreateTestFile(Diagnostic diagnostic)
+		{
+			var file = Path.Combine(GetTestsProjectPath(), diagnostic.Name + "Tests.cs");
+			var content = Templatize(diagnostic, GetTestTemplate());
+			File.WriteAllText(file, content);
+		}
+
+		private static string GetStringsResxPath()
+		{
+			return Path.Combine(GetProjectPath(), "Resources", "Strings.resx");
+		}
+
+		private static string GetProjectPath()
+		{
+			return Path.Combine(GetSourcePath(), "Microsoft.Unity.Analyzers");
+		}
+
+		private static string GetTestsProjectPath()
+		{
+			return Path.Combine(GetSourcePath(), "Microsoft.Unity.Analyzers.Tests");
+		}
+
+		private static string GetSourcePath()
+		{
+			return Path.GetFullPath(Path.Combine(Path.GetDirectoryName(typeof(Program).Module.FullyQualifiedName), "..", "..", "..", ".."));
+		}
+
+		private static string[] GetAllSourceFiles()
+		{
+			return Directory.GetFiles(GetProjectPath(), "*.cs", SearchOption.AllDirectories);
+		}
+
+		private string GetNextId()
+		{
+			int identifier = 0;
+
+			foreach (var file in GetAllSourceFiles())
+			{
+				if (TryGetAnalyzerIdentifier(file, out int id))
+					identifier = Math.Max(identifier, id);
+			}
+
+			identifier++;
+
+			return $"{IdPrefix}{identifier:D4}";
+		}
+
+		private bool TryGetAnalyzerIdentifier(string file, out int identifier)
+		{
+			identifier = -1;
+
+			using (var fs = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+			{
+				using (var reader = new StreamReader(fs))
+				{
+					string line;
+					while ((line = reader.ReadLine()) != null)
+					{
+						if (TryExtractIdentifier(line, out int id))
+						{
+							identifier = Math.Max(identifier, id);
+						}
+					}
+				}
+			}
+
+			return identifier != -1;
+		}
+
+		private bool TryExtractIdentifier(string line, out int identifier)
+		{
+			var declarationStart = $"\"{IdPrefix}";
+			identifier = -1;
+
+			var index = line.LastIndexOf(declarationStart, StringComparison.Ordinal);
+			if (index < 0)
+				return false;
+
+			index++; // "
+
+			var end = line.IndexOf("\"", ++index, StringComparison.Ordinal);
+
+			index += 3;
+
+			var id = line.Substring(index, end - index);
+
+			return int.TryParse(id, out identifier);
+		}
+
+	}
+}
---
+++ src/new-analyzer/AnalyzerCodeFixDiagnosticBuilder.cs
@@ -0,0 +1,139 @@
+﻿/*--------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *-------------------------------------------------------------------------------------------*/
+
+namespace NewAnalyzer
+{
+	internal class AnalyzerCodeFixDiagnosticBuilder : AbstractDiagnosticBuilder
+	{
+		protected override string IdPrefix => "UNT";
+		
+		protected override string GetResourceTemplate()
+		{
+			return @"  <data name=""$(DiagnosticName)CodeFixTitle"" xml:space=""preserve"">
+    <value>TODO: Add code fix title</value>
+  </data>
+  <data name=""$(DiagnosticName)DiagnosticDescription"" xml:space=""preserve"">
+    <value>TODO: Add description.</value>
+  </data>
+  <data name=""$(DiagnosticName)DiagnosticMessageFormat"" xml:space=""preserve"">
+    <value>TODO: Add Message format</value>
+  </data>
+  <data name=""$(DiagnosticName)DiagnosticTitle"" xml:space=""preserve"">
+    <value>TODO: Add title</value>
+  </data>
+</root>";
+		}
+
+		protected override string GetCodeTemplate()
+		{
+			return @"/*--------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *-------------------------------------------------------------------------------------------*/
+
+using System.Collections.Immutable;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.Unity.Analyzers.Resources;
+
+namespace Microsoft.Unity.Analyzers
+{
+	[DiagnosticAnalyzer(LanguageNames.CSharp)]
+	public class $(DiagnosticName)Analyzer : DiagnosticAnalyzer
+	{
+		internal static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(
+			id: ""$(DiagnosticId)"",
+			title: Strings.$(DiagnosticName)DiagnosticTitle,
+			messageFormat: Strings.$(DiagnosticName)DiagnosticMessageFormat,
+			category: DiagnosticCategory._FIXME_,
+			defaultSeverity: DiagnosticSeverity.Info,
+			isEnabledByDefault: true,
+			description: Strings.$(DiagnosticName)DiagnosticDescription);
+
+		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+		public override void Initialize(AnalysisContext context)
+		{
+			context.EnableConcurrentExecution();
+			context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+			
+			// TODO: context.RegisterSyntaxNodeAction
+			// example: context.RegisterSyntaxNodeAction(AnalyzeMethodDeclaration, SyntaxKind.MethodDeclaration);
+		}
+	}
+
+	[ExportCodeFixProvider(LanguageNames.CSharp)]
+	public class $(DiagnosticName)CodeFix : CodeFixProvider
+	{
+		public sealed override ImmutableArray<string> FixableDiagnosticIds => ImmutableArray.Create($(DiagnosticName)Analyzer.Rule.Id);
+
+		public sealed override FixAllProvider GetFixAllProvider() => WellKnownFixAllProviders.BatchFixer;
+
+		public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+		{
+			// var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+
+			// var declaration = root.FindNode(context.Span) as MethodDeclarationSyntax;
+			// if (declaration == null)
+			//     return;
+
+			// context.RegisterCodeFix(
+			//     CodeAction.Create(
+			//         Strings.$(DiagnosticName)CodeFixTitle,
+			//         ct => {},
+			//         declaration.ToFullString()),
+			//     context.Diagnostics);
+		}
+	}
+}
+";
+		}
+
+		protected override string GetTestTemplate()
+		{
+			return @"/*--------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *-------------------------------------------------------------------------------------------*/
+
+using Xunit;
+
+namespace Microsoft.Unity.Analyzers.Tests
+{
+	public class $(DiagnosticName)Tests : BaseCodeFixVerifierTest<$(DiagnosticName)Analyzer, $(DiagnosticName)CodeFix>
+	{
+		[Fact]
+		public void Test()
+		{
+			const string test = @""
+using UnityEngine;
+
+class Camera : MonoBehaviour
+{
+}
+"";
+
+			var diagnostic = ExpectDiagnostic();
+
+			VerifyCSharpDiagnostic(test, diagnostic);
+
+			const string fixedTest = @""
+using UnityEngine;
+
+class Camera : MonoBehaviour
+{
+}
+"";
+
+			VerifyCSharpFix(test, fixedTest);
+		}
+	}
+}
+";
+		}
+	}
+}
