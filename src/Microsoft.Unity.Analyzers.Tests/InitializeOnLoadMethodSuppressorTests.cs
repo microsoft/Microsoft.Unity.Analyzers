b26d44d8b90ee32174153c57a8a915ff1d8f5aac
Sebastien Lebreton <sebastien@lebreton.fr>
2020-04-21 13:42:32 -0700

    Add InitializeOnLoadMethodAttribute suppressor

---
+++ src/Microsoft.Unity.Analyzers.Tests/InitializeOnLoadMethodSuppressorTests.cs
@@ -0,0 +1,54 @@
+/*--------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *-------------------------------------------------------------------------------------------*/
+
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Microsoft.Unity.Analyzers.Tests
+{
+	public class InitializeOnLoadMethodSuppressorTests : BaseSuppressorVerifierTest<InitializeOnLoadMethodSuppressor>
+	{
+		[Fact]
+		public async Task InitializeOnLoadMethodTest()
+		{
+			const string test = @"
+using UnityEditor;
+
+class Loader
+{
+    [InitializeOnLoadMethod]
+    private static void OnLoad() {
+    }
+}
+";
+
+			var suppressor = ExpectSuppressor(InitializeOnLoadMethodSuppressor.Rule)
+				.WithLocation(7, 25);
+
+			await VerifyCSharpDiagnosticAsync(test, suppressor);
+		}
+
+		[Fact]
+		public async Task RuntimeInitializeOnLoadMethodTest()
+		{
+			const string test = @"
+using UnityEngine;
+
+class Loader
+{
+    [RuntimeInitializeOnLoadMethod]
+    private static void OnLoad() {
+    }
+}
+";
+
+			var suppressor = ExpectSuppressor(InitializeOnLoadMethodSuppressor.Rule)
+				.WithLocation(7, 25);
+
+			await VerifyCSharpDiagnosticAsync(test, suppressor);
+		}
+
+	}
+}
