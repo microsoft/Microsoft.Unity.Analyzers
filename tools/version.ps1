b54c3ab4781aaa4e6f641f586e67ff8bdf636fcb
Sebastien Lebreton <sebastien@lebreton.fr>
2020-06-05 11:03:13 -0700

    Add version tool

---
+++ tools/version.ps1
@@ -0,0 +1,16 @@
+function Process([string[]]$files, [string]$search, [string]$replace) 
+{
+	foreach ($file in Get-ChildItem $files) {
+		(Get-Content $file) | 
+		Foreach-Object {$_ -replace $search, $replace}  | 
+		Out-File -Encoding UTF8 $file
+	 }
+}
+
+$VERSION_SHORT = "1.7.0"
+$VERSION_LONG = $VERSION_SHORT + ".0"
+$VERSIONSEARCH_SHORT = "\d+\.\d+\.\d+"
+$VERSIONSEARCH_LONG = $VERSIONSEARCH_SHORT + "\.\d+"
+
+Process ("libs/ProductInfo.cs") ($VERSIONSEARCH_LONG) ($VERSION_LONG)
+Process ("build/UnityVS.props") ('<VersionPrefix>' + $VERSIONSEARCH_SHORT) ('<VersionPrefix>' + $VERSION_SHORT)
