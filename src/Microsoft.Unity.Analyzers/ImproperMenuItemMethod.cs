1f3ba98d5b35f100c9397950922b1527824984a6
shreyalpandit <46079670+shreyalpandit@users.noreply.github.com>
2021-01-13 22:51:31 -0700

    Update ImproperMenuItemMethod.cs

--- src/Microsoft.Unity.Analyzers/ImproperMenuItemMethod.cs
+++ src/Microsoft.Unity.Analyzers/ImproperMenuItemMethod.cs
@@ -71,8 +71,6 @@ namespace Microsoft.Unity.Analyzers
 			var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
 
 			if (!(root?.FindNode(context.Span) is MethodDeclarationSyntax declaration))
-				return;			
-			if (declaration == null)
 				return;
 
 			context.RegisterCodeFix(
