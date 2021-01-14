2f678841e8696baec3d53607350f1a334ef879fd
Sebastien Lebreton <sebastien@lebreton.fr>
2021-01-14 15:47:51 -0800

    Remove unused ns

--- src/Microsoft.Unity.Analyzers/IndirectionMessage.cs
+++ src/Microsoft.Unity.Analyzers/IndirectionMessage.cs
@@ -3,10 +3,7 @@
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *-------------------------------------------------------------------------------------------*/
 
-using System;
-using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
