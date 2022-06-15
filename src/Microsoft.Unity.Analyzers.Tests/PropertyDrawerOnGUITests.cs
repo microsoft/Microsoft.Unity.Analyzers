50862ded94ad0667e1620e463267b0ea3c9c0d89
Sebastien Lebreton <sebastien@lebreton.fr>
2022-06-15 21:31:09 +0200

    Add UNT0027 Diagnostic/CodeFix (#221)

---
+++ src/Microsoft.Unity.Analyzers.Tests/PropertyDrawerOnGUITests.cs
@@ -0,0 +1,126 @@
+/*--------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *-------------------------------------------------------------------------------------------*/
+
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Microsoft.Unity.Analyzers.Tests;
+
+public class PropertyDrawerOnGUITests : BaseCodeFixVerifierTest<PropertyDrawerOnGUIAnalyzer, PropertyDrawerOnGUICodeFix>
+{
+	[Fact]
+	public async Task TestOnGUI()
+	{
+		const string test = @"
+using UnityEditor;
+
+class MyDrawer : PropertyDrawer
+{
+	public void Foo() {
+		OnGUI(default, default, default);
+	}
+}
+";
+
+		var diagnostic = ExpectDiagnostic()
+			.WithLocation(7, 3);
+
+		await VerifyCSharpDiagnosticAsync(test, diagnostic);
+
+		const string fixedTest = @"
+using UnityEditor;
+
+class MyDrawer : PropertyDrawer
+{
+	public void Foo() {
+	}
+}
+";
+
+		await VerifyCSharpFixAsync(test, fixedTest);
+	}
+
+	[Fact]
+	public async Task TestOnGUIOverride()
+	{
+		const string test = @"
+using UnityEngine;
+using UnityEditor;
+
+class DerivedDrawer : PropertyDrawer
+{
+	public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+	}
+}
+
+class MyDrawer : DerivedDrawer
+{
+	public void Foo() {
+		OnGUI(default, default, default);
+	}
+}
+";
+
+		await VerifyCSharpDiagnosticAsync(test);
+	}
+
+	[Fact]
+	public async Task TestBaseOnGUI()
+	{
+		const string test = @"
+using UnityEngine;
+using UnityEditor;
+
+class MyDrawer : PropertyDrawer
+{
+	public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+		base.OnGUI(position, property, label);
+	}
+}
+";
+
+		var diagnostic = ExpectDiagnostic()
+			.WithLocation(8, 3);
+
+		await VerifyCSharpDiagnosticAsync(test, diagnostic);
+
+		const string fixedTest = @"
+using UnityEngine;
+using UnityEditor;
+
+class MyDrawer : PropertyDrawer
+{
+	public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+	}
+}
+";
+
+		await VerifyCSharpFixAsync(test, fixedTest);
+	}
+
+	[Fact]
+	public async Task TestBaseOnGUIOverride()
+	{
+		const string test = @"
+using UnityEngine;
+using UnityEditor;
+
+class DerivedDrawer : PropertyDrawer
+{
+	public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+	}
+}
+
+class MyDrawer : DerivedDrawer
+{
+	public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+		base.OnGUI(position, property, label);
+	}
+}
+";
+
+		await VerifyCSharpDiagnosticAsync(test);
+	}
+}
---
+++ src/Microsoft.Unity.Analyzers/PropertyDrawerOnGUI.cs
@@ -0,0 +1,90 @@
+/*--------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *-------------------------------------------------------------------------------------------*/
+
+using System.Collections.Immutable;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.Unity.Analyzers.Resources;
+
+namespace Microsoft.Unity.Analyzers;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public class PropertyDrawerOnGUIAnalyzer : DiagnosticAnalyzer
+{
+	internal static readonly DiagnosticDescriptor Rule = new(
+		id: "UNT0027",
+		title: Strings.PropertyDrawerOnGUIDiagnosticTitle,
+		messageFormat: Strings.PropertyDrawerOnGUIDiagnosticMessageFormat,
+		category: DiagnosticCategory.Correctness,
+		defaultSeverity: DiagnosticSeverity.Info,
+		isEnabledByDefault: true,
+		description: Strings.PropertyDrawerOnGUIDiagnosticDescription);
+
+	public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+	public override void Initialize(AnalysisContext context)
+	{
+		context.EnableConcurrentExecution();
+		context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+
+		context.RegisterSyntaxNodeAction(AnalyzeInvocation, SyntaxKind.InvocationExpression);
+	}
+
+	private static void AnalyzeInvocation(SyntaxNodeAnalysisContext context)
+	{
+		var invocation = (InvocationExpressionSyntax)context.Node;
+		var name = invocation.GetMethodNameSyntax();
+		if (name is not {Identifier.Text: "OnGUI"})
+			return;
+
+		var symbol = context.SemanticModel.GetSymbolInfo(invocation);
+
+		if (symbol.Symbol is not IMethodSymbol method)
+			return;
+
+		if (method.ContainingType.ToDisplayString() != "UnityEditor.PropertyDrawer")
+			return;
+
+		context.ReportDiagnostic(Diagnostic.Create(Rule, invocation.GetLocation(), invocation));
+	}
+}
+
+[ExportCodeFixProvider(LanguageNames.CSharp)]
+public class PropertyDrawerOnGUICodeFix : CodeFixProvider
+{
+	public sealed override ImmutableArray<string> FixableDiagnosticIds => ImmutableArray.Create(PropertyDrawerOnGUIAnalyzer.Rule.Id);
+
+	public sealed override FixAllProvider GetFixAllProvider() => WellKnownFixAllProviders.BatchFixer;
+
+	public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+	{
+		var invocation = await context.GetFixableNodeAsync<InvocationExpressionSyntax>();
+		if (invocation == null)
+			return;
+
+		context.RegisterCodeFix(
+			CodeAction.Create(
+				Strings.PropertyDrawerOnGUICodeFixTitle,
+				ct => RemoveInvocationAsync(context.Document, invocation, ct),
+				invocation.ToFullString()),
+			context.Diagnostics);
+	}
+
+	private static async Task<Document> RemoveInvocationAsync(Document document, InvocationExpressionSyntax invocation, CancellationToken cancellationToken)
+	{
+		var root = await document.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
+		var newRoot = root.RemoveNode(invocation.Parent, SyntaxRemoveOptions.KeepNoTrivia);
+		if (newRoot == null)
+			return document;
+
+		return document.WithSyntaxRoot(newRoot);
+	}
+}
