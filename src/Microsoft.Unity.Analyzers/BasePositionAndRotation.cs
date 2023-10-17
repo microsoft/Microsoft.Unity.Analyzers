0682322a186a8a50a995fd77d7a0ea9e785d0884
Sebastien Lebreton <sebastien@lebreton.fr>
2023-10-17 15:33:01 +0200

    Remove unused using

--- src/Microsoft.Unity.Analyzers/BasePositionAndRotation.cs
+++ src/Microsoft.Unity.Analyzers/BasePositionAndRotation.cs
@@ -7,7 +7,6 @@ using System;
 using System.Collections.Immutable;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
