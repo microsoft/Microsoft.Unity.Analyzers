2e60bb517a13e3a7fbd99cc60b4ab407d985dbb1
Sebastien Lebreton <sebastien@lebreton.fr>
2024-08-30 10:21:10 +0200

    Update suppressions

--- src/Microsoft.Unity.Analyzers/Nullability.cs
+++ src/Microsoft.Unity.Analyzers/Nullability.cs
@@ -3,9 +3,7 @@
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *-------------------------------------------------------------------------------------------*/
 
-#pragma warning disable IDE0130
 namespace System.Diagnostics.CodeAnalysis;
-#pragma warning restore IDE0130
 
 [AttributeUsage(AttributeTargets.Parameter)]
 internal sealed class NotNullWhenAttribute(bool returnValue) : Attribute
