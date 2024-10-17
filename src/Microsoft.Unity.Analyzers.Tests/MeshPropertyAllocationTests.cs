66cf696907e334e214c8b513e4c006054f500c69
Kisaragi Marine <kisaragi.effective@gmail.com>
2024-10-17 14:41:45 +0900

    add lint: Allocating properties of Mesh should not be get in loop

---
+++ src/Microsoft.Unity.Analyzers.Tests/MeshPropertyAllocationTests.cs
@@ -0,0 +1,32 @@
+/*--------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *-------------------------------------------------------------------------------------------*/
+
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Microsoft.Unity.Analyzers.Tests;
+
+public class MeshPropertyAllocationTests : BaseCodeFixVerifierTest<MeshPropertyAllocationAnalyzer, MeshPropertyAllocationCodeFix>
+{
+	[Fact]
+	public async Task Test()
+	{
+		const string test = @"
+class DummyClass
+{
+    private void Test(UnityEngine.Mesh mesh) {
+        for (var i = 0; i <= 5; i++) {
+            var c = mesh.uv;
+        }
+    }
+}
+";
+
+		var diagnostic = ExpectDiagnostic(MeshPropertyAllocationAnalyzer.Rule)
+			.WithLocation(6, 21);
+
+		await VerifyCSharpDiagnosticAsync(test, diagnostic);
+	}
+}
