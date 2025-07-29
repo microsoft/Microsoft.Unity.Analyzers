45340edde2cc4079fae98737f6ea30b7d1fdf01b
Sebastien Lebreton <sebleb@microsoft.com>
2025-07-29 11:52:11 +0200

    Retract namespace

--- src/Microsoft.Unity.Analyzers/RequireComponent.cs
+++ src/Microsoft.Unity.Analyzers/RequireComponent.cs
@@ -4,7 +4,6 @@
  *-------------------------------------------------------------------------------------------*/
 
 using System.Collections.Immutable;
-using System.Data;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
