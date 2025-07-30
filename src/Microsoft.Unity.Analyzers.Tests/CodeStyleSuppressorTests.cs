be8745f3b9e505207fe276e93b9c7e77e165d33d
Sebastien Lebreton <sebleb@microsoft.com>
2025-07-30 09:37:31 +0200

    Found the test issue

--- src/Microsoft.Unity.Analyzers.Tests/CodeStyleSuppressorTests.cs
+++ src/Microsoft.Unity.Analyzers.Tests/CodeStyleSuppressorTests.cs
@@ -8,21 +8,31 @@ using Xunit;
 
 namespace Microsoft.Unity.Analyzers.Tests;
 
-public class CodeStyleSuppressorTests
+public class CodeStyleSuppressorTests : BaseSuppressorVerifierTest<CodeStyleSuppressor>
 {
 	[Fact]
-	public Task TestCodeStyleIgnore()
+	public async Task TestCodeStyleIgnore()
 	{
-		/* This suppressor is used to ignore the IDE1006 diagnostic (naming conventions) for methods
-		 * that are part of Unity's MonoBehaviour lifecycle methods, such as Start, Update, etc.
-		 * It is intended to be used in Unity projects where these method names are standard, even
-		 * when using camel case naming conventions.
-		 * 
-		 * It was manually tested in both VS ans VSCode, but we don't have an automated test for it.
-		 * As soon as we expose the suppressor to the test infrastructure, we hit a "NonImplementedExcetion".
-		 * I guess this is because the naming analyzer is one of the remaining components that are hosted
-		 * in the IDE and not directly in Roslyn. */
+		const string test = @"
+using UnityEngine;
 
-		return Task.CompletedTask;
+class Menu : MonoBehaviour
+{
+    private void Update() { }
+    public void Start() { }
+}";
+
+		var context = AnalyzerVerificationContext.Default
+			.WithAnalyzerOption("dotnet_naming_style.camel_case.capitalization", "camel_case")
+			.WithAnalyzerOption("dotnet_naming_symbols.private_methods.applicable_kinds", "method")
+			.WithAnalyzerOption("dotnet_naming_symbols.private_methods.applicable_accessibilities", "private")
+			.WithAnalyzerOption("dotnet_naming_rule.private_methods.symbols", "private_methods")
+			.WithAnalyzerOption("dotnet_naming_rule.private_methods.style", "camel_case")
+			.WithAnalyzerOption("dotnet_naming_rule.private_methods.severity", "warning");
+
+		var suppressor = ExpectSuppressor(CodeStyleSuppressor.Rule)
+			.WithLocation(6, 18);
+
+		await VerifyCSharpDiagnosticAsync(context, test, suppressor);
 	}
 }
