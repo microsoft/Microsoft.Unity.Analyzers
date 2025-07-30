406a5f52ddfa1dfc1a201c026917b2f59664fc86
Sebastien Lebreton <sebleb@microsoft.com>
2025-07-30 10:09:45 +0200

    Refine tests

--- src/Microsoft.Unity.Analyzers.Tests/CodeStyleSuppressorTests.cs
+++ src/Microsoft.Unity.Analyzers.Tests/CodeStyleSuppressorTests.cs
@@ -4,6 +4,7 @@
  *-------------------------------------------------------------------------------------------*/
 
 using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
 using Xunit;
 
 namespace Microsoft.Unity.Analyzers.Tests;
@@ -11,7 +12,7 @@ namespace Microsoft.Unity.Analyzers.Tests;
 public class CodeStyleSuppressorTests : BaseSuppressorVerifierTest<CodeStyleSuppressor>
 {
 	[Fact]
-	public async Task TestCodeStyleIgnore()
+	public async Task TestCodeStyleIgnoreForUnityMessages()
 	{
 		const string test = @"
 using UnityEngine;
@@ -34,5 +35,34 @@ class Menu : MonoBehaviour
 			.WithLocation(6, 18);
 
 		await VerifyCSharpDiagnosticAsync(context, test, suppressor);
+	}
+
+	[Fact]
+	public async Task TestCodeStyleEffectiveForNonUnityMessages()
+	{
+		const string test = @"
+using UnityEngine;
+
+class Menu : MonoBehaviour
+{
+    private void Foo() { }
+    public void Bar() { }
+}";
+
+		var context = AnalyzerVerificationContext.Default
+			.WithAnalyzerOption("dotnet_naming_style.camel_case.capitalization", "camel_case")
+			.WithAnalyzerOption("dotnet_naming_symbols.private_methods.applicable_kinds", "method")
+			.WithAnalyzerOption("dotnet_naming_symbols.private_methods.applicable_accessibilities", "private")
+			.WithAnalyzerOption("dotnet_naming_rule.private_methods.symbols", "private_methods")
+			.WithAnalyzerOption("dotnet_naming_rule.private_methods.style", "camel_case")
+			.WithAnalyzerOption("dotnet_naming_rule.private_methods.severity", "warning");
+
+		var not = ExpectNotSuppressed(CodeStyleSuppressor.Rule)
+				.WithLocation(6, 18)
+				.WithSeverity(DiagnosticSeverity.Info)
+				.WithMessageFormat("Naming rule violation: The first word, '{0}', must begin with a lower case character")
+				.WithArguments("Foo");
+
+		await VerifyCSharpDiagnosticAsync(context, test, not);
 	}
 }
